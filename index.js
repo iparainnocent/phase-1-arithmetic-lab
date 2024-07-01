576e93b9a31d3c2d64331a069dde51457f8c56a7
iparainnocent <innocent.ipara@student.moringaschool.com>
2024-07-01 08:52:47 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const num1 = 31;
 
-// Write your code here
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 9;
+
+const num4 = 5;
+
+const mod = num3 % num4;
+
+
+
+const numbers = [10, 6, 5, 20];
+
+const max = Math.max(...numbers);
+
+
+
